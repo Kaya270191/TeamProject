a7ca1f8dcd1272058e0af8933fc24ded974213d6
merlindoraji <hio201140@gmail.com>
2022-04-06 13:53:14 +0900

    GDH_main_update_0406

---
+++ src/main/resources/static/js/styleJS/main.js
@@ -0,0 +1,23 @@
+const whiteQuotes = 'url("image/quotes/quotes-w.png")';
+const grayQuotes = 'url("image/quotes/quotes-g.png")';
+
+const whiteLogo = 'url("/image/vsc-white.png")';
+const blackLogo = 'url("/image/vsc-black.png")';
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > screen.height*0.7) {
+        $(".quotes").css("background-image" , grayQuotes);
+        $(".top-nav-logo").css("background-image" , blackLogo);
+        $(".nav-container").css("background" , "linear-gradient( 45deg, #eee, #eee )");
+        $(".nav-container").css("opacity" , "1");
+      }
+      else{
+        $(".quotes").css("background-image" , whiteQuotes);
+        $(".top-nav-logo").css("background-image" , whiteLogo);
+        $(".nav-container").css("background" , "linear-gradient( 45deg, #85364A, #505050 )");
+        $(".nav-container").css("opacity" , "0.8");
+      }
+    })
+  })
