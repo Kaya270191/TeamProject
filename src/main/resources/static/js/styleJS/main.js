6b92e30ab6c5a63f50bad2435a0094f36e7c726a
merlindoraji <hio201140@gmail.com>
2022-04-06 14:36:24 +0900

    GDH_main_update_0406_2

--- src/main/resources/static/js/styleJS/main.js
+++ src/main/resources/static/js/styleJS/main.js
@@ -8,12 +8,14 @@ $(document).ready(function(){
     $(window).scroll(function(){
       var scroll = $(window).scrollTop();
       if (scroll > screen.height*0.7) {
+        $(".sports-wrap").css("opacity" , "1");
         $(".quotes").css("background-image" , grayQuotes);
         $(".top-nav-logo").css("background-image" , blackLogo);
         $(".nav-container").css("background" , "linear-gradient( 45deg, #eee, #eee )");
         $(".nav-container").css("opacity" , "1");
       }
       else{
+        $(".sports-wrap").css("opacity" , "0");
         $(".quotes").css("background-image" , whiteQuotes);
         $(".top-nav-logo").css("background-image" , whiteLogo);
         $(".nav-container").css("background" , "linear-gradient( 45deg, #85364A, #505050 )");
@@ -21,3 +23,26 @@ $(document).ready(function(){
       }
     })
   })
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > screen.height*0.2) {
+      $(".sports-wrap").css("opacity" , "1");
+
+    }
+    else{
+      $(".sports-wrap").css("opacity" , "0");
+
+    }
+  })
+})
+
+var str = "only way to prove that you’re a good sport is to lose\"";
+var index = 0;
+
+setInterval(function () {
+  if (index < str.length)
+      $('#typing').append(str[index]);
+  index++;
+}, 60);
